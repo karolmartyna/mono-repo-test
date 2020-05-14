bfa8074333fc7d36453e648b2a335b423d762e23
BartekDymowski <bart.dy@gmail.com>
2020-05-14 17:24:06 +0200

    remove

--- land-queue/land_queue/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-class Config:
-    GH_APP_SLUG = os.environ['GH_APP_SLUG']
-    GH_APP_ID = os.environ['GH_APP_ID']
-    GH_APP_KEY = os.environ['GH_APP_KEY']
--- land-queue/land_queue/handlers/installation_handler.py
+++
@@ -1,8 +0,0 @@
-import json
-
-
-class InstallationHandler:
-    def process(self, data):
-        payload = json.loads(data)
-        action = payload['action']
-        print(f"Processing installation: {action}")
